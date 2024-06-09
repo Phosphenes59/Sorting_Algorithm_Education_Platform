fe4adf47b4d679d52aa16cfd238469d79a04da12
jessica <w19850336668@163.com>
2024-06-09 16:26:49 +0800

    修改findCurrList和findSolution

--- src/main/java/com/example/sorting_algorithm_education_platform/mapper/BubbleSortMapper.java
+++ src/main/java/com/example/sorting_algorithm_education_platform/mapper/BubbleSortMapper.java
@@ -4,11 +4,13 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.example.sorting_algorithm_education_platform.entity.BubbleSort;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
 public interface BubbleSortMapper extends BaseMapper<BubbleSort> {
     BubbleSort findNextStep(Integer practiceId, Integer processNum);
-    String findCurrList(Integer practiceId, Integer processNum);
-    String findSolution(Integer practiceId);
+    String findCurrList(Integer userId, Integer practiceId, Integer processNum);
+    List<BubbleSort> findSolution(Integer userId, Integer practiceId);
     void insertSort(BubbleSort bubbleSort);
     void deleteSort(Integer practiceId, Integer userId);
     int countByPracticeIdAndUserId(Integer practiceId, Integer userId);
--- src/main/java/com/example/sorting_algorithm_education_platform/service/BubbleSortService.java
+++ src/main/java/com/example/sorting_algorithm_education_platform/service/BubbleSortService.java
@@ -1,12 +1,16 @@
 package com.example.sorting_algorithm_education_platform.service;
 
 import com.example.sorting_algorithm_education_platform.entity.BubbleSort;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 public interface BubbleSortService {
     BubbleSort getByBubbleId(Integer bubbleId);
     BubbleSort getNextStep(Integer practiceId, Integer processNum);
-    String getCurrList(Integer practiceId, Integer processNum);
-    String getSolution(Integer practiceId);
+    String getCurrList(Integer userId, Integer practiceId, Integer processNum);
+    List<BubbleSort> getSolution(Integer userId, Integer practiceId);
     void insertSort(BubbleSort bubbleSort);
     void deleteSort(Integer practiceId, Integer userId);
     int countByPracticeIdAndUserId(Integer practiceId, Integer userId);
